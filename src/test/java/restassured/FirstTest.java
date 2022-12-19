406800dfe0292b712245c89bdf844f072b0ad9bb
Hüseyin ÇAMCI <hcamci.ce@gmail.com>
2022-12-19 14:05:26 +0300

    add new test

--- src/test/java/restassured/FirstTest.java
+++ src/test/java/restassured/FirstTest.java
@@ -1,6 +1,8 @@
 package restassured;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -9,16 +11,24 @@ import static org.hamcrest.Matchers.equalTo;
 public class FirstTest {
 
     @Test
-    public void getCall() {
-            given()
-                    .when()
-                    .get("https://api.zippopotam.us/us/90210")
-                    .then()
-                    .statusCode(200)
-                    .assertThat()
-                    .contentType(ContentType.JSON)
-                    .body("country", equalTo("United States"));
+    public void api_returns_200_with_us_and_expected_post_code() {
+        given()
+                .when()
+                .get("https://api.zippopotam.us/us/90210")
+                .then()
+                .statusCode(200)
+                .assertThat()
+                .contentType(ContentType.JSON)
+                .body("country", equalTo("United States"));
     }
 
 
+    @Test
+    public void api_returns_404_with_us_and_expected_post_code() {
+        given()
+                .when()
+                .get("https://api.zippopotam.us/us/902101111")
+                .then()
+                .statusCode(404);
+    }
 }
